96e0275d8ad7fcc6eced361fc46f5b132687e98a
RobinsonNguyen <robinson_nguyen@hotmail.com>
2023-02-24 01:35:32 +0900

    completed assessment

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,16 +1,59 @@
+import { useState, useEffect } from "react";
+
 const Comments = () => {
-    // Insturction:
-    // Use endpoint: https://jsonplaceholder.typicode.com/comments to get a list of comments. 
-    // display the comment body on the screen
-    // add event listeners to each comment body that when clicked will simply console.log the comment id.
+  const [comments, setComments] = useState([]);
+  // Insturction:
+  // Use endpoint: https://jsonplaceholder.typicode.com/comments to get a list of comments.
+  // display the comment body on the screen
+  // add event listeners to each comment body that when clicked will simply console.log the comment id.
 
-    // you will need to use the useEffect hook. Do this in async / await syntax.
+  // you will need to use the useEffect hook. Do this in async / await syntax.
+  const getComments = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    );
+    const data = await response.json();
+    setComments(data);
+  };
 
+  useEffect(() => {
+    getComments();
+  }, []);
 
-    return(
-        // Replace this with your code.
-        <h1>Comments component</h1>
-    )
-}
+  const handleClick = (e) => {
+    console.log(e.target);
+  };
 
-export default Comments
+  return (
+    <div>
+      {comments.map((element) => {
+        return (
+          <Comment
+            key={element.id}
+            id={element.id}
+            name={element.name}
+            email={element.email}
+            body={element.body}
+            onClick={handleClick}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+const Comment = (props) => {
+  const handleClick = (e) => {
+    console.log(e.target.id);
+  };
+  return (
+    <div>
+      <br></br>
+      <h2>{props.email}</h2>
+      <div id={props.id} onClick={handleClick}>
+        {props.body}
+      </div>
+    </div>
+  );
+};
+export default Comments;
